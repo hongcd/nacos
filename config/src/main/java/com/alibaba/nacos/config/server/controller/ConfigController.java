f412779182fa157c6740fb288b759f92a962874c
candong.hong <hongcandong@gmail.com>
2021-08-19 16:18:32 +0800

    feat(all): 去除无用代码

--- config/src/main/java/com/alibaba/nacos/config/server/controller/ConfigController.java
+++ config/src/main/java/com/alibaba/nacos/config/server/controller/ConfigController.java
@@ -62,7 +62,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -117,10 +116,7 @@ public class ConfigController {
     
     @Autowired
     private ConfigSubService configSubService;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
+    
     @Autowired
     private NacosUserService nacosUserService;
     
